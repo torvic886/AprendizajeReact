a4fdb6b50e010b9a2bca33dc1efc3305470796ba
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-05 09:34:14 -0500

    Manejo de Arreglos.

---
+++ 01-intro-js/src/07-Arreglos.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+const products = ['mesa','silla','notebook','teclado'];
+
+//push agregar varios elementos a un arreglo o uno;
+products.push('pantalla lcd', 'Sony Tv');
+
+// console.log(products);
+
+// metodo 1
+// products.forEach( el => console.log(el))
+
+// metodo 2
+//metodo de referencia
+// products.forEach( console.log )
+
+// console.log(products);
+
+// metodo 3
+// for( const prod of products) {
+//     console.log(prod);
+// }
+
+// metodo 4: tradicional
+// for (let i = 0; i < products.length; i++) {
+//     // eslint-disable-next-line no-unused-vars
+//     const element = products[i];
+//     console.log(element);
+    
+// }
